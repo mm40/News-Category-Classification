8e0f792376f96f81d46b6680937b72f95aca7000
mm40 <mihailomal@gmail.com>
2020-12-13 01:16:49 +0100

    - Further improvement of ~DatasetNewsVectorized~ : working ~__init__~ and ~__getitem__~

--- utilities/loading.py
+++ utilities/loading.py
@@ -1,6 +1,9 @@
 from torch.utils.data.dataset import Dataset
 from torch.utils.data.dataloader import DataLoader
+from utilities.munging import apply_regex_list_to_text
+from utilities.nlp import SentenceTensorConverter
 from pandas import unique
+from collections import Counter
 
 
 class DatasetNews(Dataset):
@@ -40,7 +43,8 @@ class DatasetNews(Dataset):
 
 
 class DatasetNewsVectorized(DatasetNews):
-    def __init__(self, df, splitColHeader, vocabCat, vocabHeadl):
+    def __init__(self, df, splitColHeader, vocabCat, vocabHeadl, regexList,
+                 headerCategory, headerHeadline):
         """Instantiates DatasetNewsVectorized object, which is different to
             DatasetNews in that it returns vectorized result, instead of plain.
 
@@ -49,8 +53,49 @@ class DatasetNewsVectorized(DatasetNews):
             splitColHeader (str): -||-
             vocabCat (Vocabulary): vocabulary for translating category to idx
             vocabHeadl (VocabularySeq): vocabulary for sequentializing headline
+            headerCategory (str): header of category column of dataframe
+            headerHeadline (str): header of headline column of dataframe
         """
-        super().__init__(df, splitColHeader)
         self._vocabCat = vocabCat
         self._vocabHeadl = vocabHeadl
-        # TODO : determine maximum sequence width
+        self._headerCategory = headerCategory
+        self._headerHeadline = headerHeadline
+
+        class localMaxLengthKeeper():
+            """Just an integer to be increased to max value"""
+            def __init__(self):
+                self.maxLength = 0
+                # TODO: use lengthsCntr to remove too long headlines (outliers)
+                # with localTextProcess, add colun with width, for easy remove?
+                self.lengthsCntr = Counter()
+
+            def updateLength(self, newLength):
+                self.lengthsCntr[newLength] += 1
+                if newLength > self.maxLength:
+                    self.maxLength = newLength
+
+        def localTextProcess(text, keeper):
+            """Besides modidying text, this function will set longest sequence.
+            This hack is done to not iterate again for determining it later."""
+            text = apply_regex_list_to_text(regexList, text.lower())
+            keeper.updateLength(len(list(filter(None, text.split(" ")))))
+            return text
+
+        keeper = localMaxLengthKeeper()
+        df[headerHeadline] = df[headerHeadline].apply(localTextProcess,
+                                                      keeper=keeper)
+        self._maxSeqLength = keeper.maxLength
+
+        self._converter = SentenceTensorConverter(self._vocabHeadl,
+                                                  self._maxSeqLength)
+
+        super().__init__(df, splitColHeader)  # called after df.apply
+        pass
+
+    def __getitem__(self, idx):
+        """Vectorizes headline text, gets category id, returns dict of those"""
+        row = super().__getitem__(idx)
+        headlineList = row[self._headerHeadline].split(" ")
+        category = row[self._headerCategory]
+        return {"x": self._converter.tokensToIdxs(headlineList),
+                "y": self._vocabCat.idx_from_token(category)}
